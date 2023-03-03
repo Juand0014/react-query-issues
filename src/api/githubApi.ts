02c08e255c9ba1ae09dbc7db357f96f6b8a3a998
juand0014 <juand0014@hotmail.com>
2023-03-03 17:00:22 -0400

    add items

--- src/api/githubApi.ts
+++ src/api/githubApi.ts
@@ -2,5 +2,7 @@ import axios from 'axios';
 
 export const githubApi = axios.create({
 	baseURL: 'https://api.github.com/repos/facebook/react',
-	headers: {}
+	headers: {
+		Authorization: 'Bearer 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'
+	}
 })
--- src/issues/views/ListView.tsx
+++ src/issues/views/ListView.tsx
@@ -1,31 +1,33 @@
-import { useState } from 'react';
-import { IssueList } from '../components/IssueList';
-import { LabelPicker } from '../components/LabelPicker';
-
+import { useState } from "react";
+import { LoadingIcon } from "../../shared/components/LoadingIcon";
+import { IssueList } from "../components/IssueList";
+import { LabelPicker } from "../components/LabelPicker";
+import { useIssues } from "../hooks";
 
 export const ListView = () => {
+    const [selectedLabels, setSelectedLabels] = useState<string[]>([]);
+    const { issuesQuery } = useIssues();
 
-  const [selectedLabels, setSelectedLabels] = useState<string[]>([])
+    const onLabelChanged = (labelName: string) => {
+        selectedLabels.includes(labelName)
+            ? setSelectedLabels(
+                  selectedLabels.filter((label) => label !== labelName)
+              )
+            : setSelectedLabels([...selectedLabels, labelName]);
+    };
 
-  const onLabelChanged = (labelName: string) => {
-    ( selectedLabels.includes(labelName))
-      ? setSelectedLabels( selectedLabels.filter( label => label !== labelName))
-      : setSelectedLabels([...selectedLabels, labelName])
-  }
+    return (
+        <div className="row mt-5">
+            <div className="col-8">
+                {issuesQuery.isLoading ? <LoadingIcon /> : <IssueList issues={ issuesQuery.data || [] } />}
+            </div>
 
-  return (
-    <div className="row mt-5">
-      
-      <div className="col-8">
-        <IssueList />
-      </div>
-      
-      <div className="col-4">
-        <LabelPicker
-          selectedLabels={ selectedLabels }
-          onChange={(labelName)=>onLabelChanged(labelName)}
-        />
-      </div>
-    </div>
-  )
-}
+            <div className="col-4">
+                <LabelPicker
+                    selectedLabels={selectedLabels}
+                    onChange={(labelName) => onLabelChanged(labelName)}
+                />
+            </div>
+        </div>
+    );
+};
